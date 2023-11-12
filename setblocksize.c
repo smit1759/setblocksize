71c280594175c21962b160bff76568329d9b03f5
Carter Smith <carter.smith@stric.com.au>
2023-11-12 11:20:22 +0930

    Added other make

--- setblocksize.c
+++ setblocksize.c
@@ -85,11 +85,11 @@ To do:          -
 #define TIMEOUT (48000 * HZ) /* 800 minute FORMAT UNIT default timeout */
 #define BS 512               /* Default blocksize */
 #define IPR_CCB_CDB_LEN 16
-#define SG_DXFER_TO_DEV -2 /* e.g. a SCSI WRITE command */
-#define SG_IO 0x2285       /* similar effect as write() followed by read() */
+#define IPR_MAX_XFER 0x8000
+#define IPR_S_G_BUFF_ALIGNMENT 512
 const char NAME[] = "setblocksize";
 const char VER[] = "V0.2";
-
+const int cdb_size[] = {6, 10, 10, 0, 16, 12, 16, 16};
 /*
 ********************************************************************************
 * Main function
@@ -109,6 +109,7 @@ struct ipr_mode_parm_hdr
    uint8_t device_spec_parms;
    uint8_t block_desc_len;
 };
+
 struct sense_data_t
 {
    uint8_t error_code;
@@ -132,7 +133,8 @@ typedef struct sg_io_hdr_ibm
    unsigned char mx_sb_len;          /* [i] max length to write to sbp */
    unsigned short int iovec_count;   /* [i] 0 implies no scatter gather */
    unsigned int dxfer_len;           /* [i] byte count of data transfer */
-   void *dxferp;                     /* [i], [*io] points to data transfer memory or scatter gather list */
+   void *dxferp;                     /* [i], [*io] points to data transfer memory
+                    or scatter gather list */
    unsigned char *cmdp;              /* [i], [*i] points to command to perform */
    unsigned char *sbp;               /* [i], [*o] points to sense_buffer memory */
    unsigned int timeout;             /* [i] MAX_UINT->no timeout (unit: millisec) */
@@ -148,15 +150,143 @@ typedef struct sg_io_hdr_ibm
    int resid;                        /* [o] dxfer_len - actual_transferred */
    unsigned int duration;            /* [o] time taken by cmd (unit: millisec) */
    unsigned int info;                /* [o] auxiliary information */
-} sg_io_hdr_ibm;
+} sg_io_hdr_t_ibm;
 
-static void print_buf(const unsigned char *buf, size_t buf_len)
+struct df_sense_data_t
 {
-   size_t i = 0;
-   for (i = 0; i < buf_len; ++i)
-      fprintf(stdout, "%02X%s", buf[i],
-              (i + 1) % 16 == 0 ? "\r\n" : " ");
-}
+   uint8_t error_code;
+   uint8_t sense_key;
+   uint8_t add_sense_code;
+   uint8_t add_sense_code_qual;
+   uint8_t rfield;
+   uint8_t rsrvd[2];
+   uint8_t add_sense_len;
+};
+
+static int _sg_ioctl(int fd, uint8_t cdb[IPR_CCB_CDB_LEN],
+                     void *data, uint32_t xfer_len, uint32_t data_direction,
+                     struct sense_data_t *sense_data,
+                     uint32_t timeout_in_sec, int retries)
+{
+   int rc = 0;
+   sg_io_hdr_t io_hdr_t;
+   sg_iovec_t *iovec = NULL;
+   int iovec_count = 0;
+   int i;
+   int buff_len, segment_size;
+   void *dxferp;
+   uint8_t *buf;
+   struct sense_data_t sd;
+   struct df_sense_data_t *dfsdp = NULL;
+
+   /* check if scatter gather should be used */
+   if (xfer_len > IPR_MAX_XFER)
+   {
+      iovec_count = (xfer_len / IPR_MAX_XFER) + 1;
+      iovec = malloc(iovec_count * sizeof(sg_iovec_t));
+
+      buff_len = xfer_len;
+      segment_size = IPR_MAX_XFER;
+
+      for (i = 0; (i < iovec_count) && (buff_len != 0); i++)
+      {
+         posix_memalign(&(iovec[i].iov_base), IPR_S_G_BUFF_ALIGNMENT, segment_size);
+         if (data_direction == SG_DXFER_TO_DEV)
+            memcpy(iovec[i].iov_base, data + (IPR_MAX_XFER * i), segment_size);
+         iovec[i].iov_len = segment_size;
+
+         buff_len -= segment_size;
+         if (buff_len < segment_size)
+            segment_size = buff_len;
+      }
+
+      iovec_count = i;
+      dxferp = (void *)iovec;
+   }
+   else
+   {
+      iovec_count = 0;
+      dxferp = data;
+   }
+
+   for (i = 0; i < (retries + 1); i++)
+   {
+      memset(&io_hdr_t, 0, sizeof(io_hdr_t));
+      memset(&sd, 0, sizeof(struct sense_data_t));
+
+      io_hdr_t.interface_id = 'S';
+      io_hdr_t.cmd_len = cdb_size[(cdb[0] >> 5) & 0x7];
+      io_hdr_t.iovec_count = iovec_count;
+      io_hdr_t.flags = 0;
+      io_hdr_t.pack_id = 0;
+      io_hdr_t.usr_ptr = 0;
+      io_hdr_t.sbp = (unsigned char *)&sd;
+      io_hdr_t.mx_sb_len = sizeof(struct sense_data_t);
+      io_hdr_t.timeout = timeout_in_sec * 1000;
+      io_hdr_t.cmdp = cdb;
+      io_hdr_t.dxfer_direction = data_direction;
+      io_hdr_t.dxfer_len = xfer_len;
+      io_hdr_t.dxferp = dxferp;
+
+      rc = ioctl(fd, SG_IO, &io_hdr_t);
+
+      if (rc == -1 && errno == EINVAL)
+      {
+         rc = -EINVAL;
+         goto out;
+      }
+
+      if (rc == 0 && io_hdr_t.masked_status == CHECK_CONDITION)
+         rc = CHECK_CONDITION;
+      else if (rc == 0 && (io_hdr_t.host_status || io_hdr_t.driver_status))
+         rc = -EIO;
+
+      if (rc == 0 || io_hdr_t.host_status == 1)
+         break;
+   }
+
+   memset(sense_data, 0, sizeof(struct sense_data_t));
+
+   if (((sd.error_code & 0x7F) == 0x72) || ((sd.error_code & 0x7F) == 0x73))
+   {
+      dfsdp = (struct df_sense_data_t *)&sd;
+      /* change error_codes 0x72 to 0x70 and 0x73 to 0x71 */
+      sense_data->error_code = dfsdp->error_code & 0xFD;
+
+      /* Do not change the order of the next two assignments
+       * In the same uint8_t, the 4th bit of fixed format corresponds
+       * to SDAT_OVLF and the last 4 bits to sense_key.
+       */
+      sense_data->sense_key = dfsdp->sense_key & 0x0F;
+      if (dfsdp->rfield & 0x80)
+         sense_data->sense_key |= 0x10;
+
+      /* copy the other values */
+      sense_data->add_sense_code = dfsdp->add_sense_code;
+      sense_data->add_sense_code_qual = dfsdp->add_sense_code_qual;
+      sense_data->add_sense_len = 0;
+   }
+   else if (sd.error_code & 0x7F)
+   {
+      memcpy(sense_data, &sd, sizeof(struct sense_data_t));
+   }
+
+out:
+   if (iovec_count)
+   {
+      for (i = 0, buf = (uint8_t *)data; i < iovec_count; i++)
+      {
+         if (data_direction == SG_DXFER_FROM_DEV)
+            memcpy(buf, iovec[i].iov_base, iovec[i].iov_len);
+         buf += iovec[i].iov_len;
+         free(iovec[i].iov_base);
+      }
+      free(iovec);
+   }
+
+   return rc;
+};
+
 int main(int argc, char **argv)
 {
    unsigned short int bs = BS;
@@ -182,13 +312,7 @@ int main(int argc, char **argv)
    /* Parameter list with block descriptor */
    unsigned char para_list[12] = {0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
    /* new block descriptor and params from iprconfig */
-   mode_parm_hdr = (struct ipr_mode_parm_hdr *)ioctl_buffer;
-   memset(ioctl_buffer, 0, 255);
-   mode_parm_hdr->block_desc_len = sizeof(struct ipr_block_desc);
-   block_desc = (struct ipr_block_desc *)(mode_parm_hdr + 1);
-   block_desc->block_length[0] = 0x00;
-   block_desc->block_length[1] = BS >> 8;
-   block_desc->block_length[2] = BS & 0xff;
+
    /* end new logic*/
    int inquiry_data_len = sizeof(struct sg_header) + 0x06;
    int mode_select_data_len = sizeof(struct sg_header) + 0x06 + 0x0C;
@@ -450,57 +574,51 @@ command!\n");
    sghp->reply_len = sizeof(struct sg_header);
    sghp->pack_id = 0;
    sghp->twelve_byte = 0;
-   memcpy(scsi_buf + sizeof(struct sg_header), mode_select, 0x06);
-   memcpy(scsi_buf + sizeof(struct sg_header) + 6, block_desc, 0x0C);
-   /* new cdb logic*/
-   uint8_t newSize = sizeof(struct ipr_block_desc) + sizeof(struct ipr_mode_parm_hdr);
+   uint8_t sendBuffer[32];
+   // prepare params
+   int newSize = sizeof(struct ipr_block_desc) + sizeof(struct ipr_mode_parm_hdr);
+   int rc;
+   struct sense_data_t sense_data;
+   mode_parm_hdr = (struct ipr_mode_parm_hdr *)ioctl_buffer;
+   memset(ioctl_buffer, 0, 255);
+   mode_parm_hdr->block_desc_len = sizeof(struct ipr_block_desc);
+   block_desc = (struct ipr_block_desc *)(mode_parm_hdr + 1);
+   block_desc->block_length[0] = 0x00;
+   block_desc->block_length[1] = bs >> 8;
+   block_desc->block_length[2] = bs & 0xff;
+   // prepare cdb
    uint8_t cdb[IPR_CCB_CDB_LEN];
    memset(cdb, 0, IPR_CCB_CDB_LEN);
    cdb[0] = MODE_SELECT;
    cdb[1] = 0x10; /* PF = 1, SP = 0 */
    cdb[4] = newSize;
-   printf("cdb:\n");
-   print_buf(cdb, sizeof(cdb));
-   sg_io_hdr_ibm io_hdr_t;
-   struct sense_data_t sd;
-   int cdb_size[] = {6, 10, 10, 0, 16, 12, 16, 16};
-   io_hdr_t.interface_id = 'S';
-   io_hdr_t.cmd_len = cdb_size[(cdb[0] >> 5) & 0x7];
-   io_hdr_t.iovec_count = 0;
-   io_hdr_t.flags = 0;
-   io_hdr_t.pack_id = 0;
-   io_hdr_t.usr_ptr = 0;
-   io_hdr_t.sbp = (unsigned char *)&sd;
-   io_hdr_t.mx_sb_len = sizeof(struct sense_data_t);
-   io_hdr_t.timeout = TIMEOUT * 1000;
-   io_hdr_t.cmdp = cdb;
-   io_hdr_t.dxfer_direction = SG_DXFER_TO_DEV;
-   io_hdr_t.dxfer_len = newSize;
-   io_hdr_t.dxferp = ioctl_buffer;
-   printf("scsi_buf:\n");
-   print_buf(&io_hdr_t, sizeof(io_hdr_t));
-   exit(0);
-   int rc = 0;
-   /* end new cdb logic*/
+   // prepare header
+
+   rc = _sg_ioctl(sg_fd, cdb, ioctl_buffer, sizeof(ioctl_buffer), SG_DXFER_TO_DEV, &sense_data, TIMEOUT, 0);
+
+   if (rc != 0)
+      printf("Sense error: %x", sense_data);
+   /*
+   // copy to our buffer
+   memcpy(scsi_buf + sizeof(struct sg_header), cdb, sizeof(cdb));
+   memcpy(scsi_buf + sizeof(struct sg_header) + sizeof(cdb), ioctl_buffer, sizeof(ioctl_buffer));
 
    printf("   Done.\n");
    printf("Send MODE SELECT command ...\n");
-
-   printf("New size: %x", newSize);
-   printf("Old size: %x", mode_select_data_len);
+   printf("New size: %x\n", newSize);
+   printf("Old size: %x\n", mode_select_data_len);
    fflush(stdout);
    // old: write(sg_fd, scsi_buf, mode_select_data_len)
    // sizeof(struct ipr_block_desc) + sizeof(struct ipr_mode_parm_hdr)
-   rc = ioctl(sg_fd, SG_IO, &io_hdr_t);
-
-   if (write(sg_fd, &io_hdr_t, sizeof(io_hdr_t)) < 0)
+   if (write(sg_fd, scsi_buf, newSize) < 0)
    {
       fprintf(stderr, "   Write error\n\n");
       close(sg_fd);
       exit(1);
    }
+   */
    /* Read status (sense_buffer) */
-   if (read(sg_fd, scsi_buf, sizeof(struct sg_io_hdr_ibm)) < 0)
+   if (read(sg_fd, scsi_buf, sizeof(struct sg_header)) < 0)
    {
       fprintf(stderr, "   Read error\n\n");
       close(sg_fd);
@@ -538,7 +656,7 @@ command!\n");
       close(sg_fd);
       exit(1);
    }
-   exit(0);
+
    /* Send FORMAT UNIT command */
    printf("Prepare command ...\n");
    fflush(stdout);
