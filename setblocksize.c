c5e576437e7adf3cc366bbf52209ad9c143a0a06
Carter Smith <carter.smith@stric.com.au>
2023-11-19 15:25:00 +0930

    Added other make

--- setblocksize.c
+++ setblocksize.c
@@ -257,9 +257,6 @@ static int _sg_ioctl(int fd, uint8_t cdb[IPR_CCB_CDB_LEN],
       io_hdr_t.dxfer_direction = data_direction;
       io_hdr_t.dxfer_len = xfer_len;
       io_hdr_t.dxferp = dxferp;
-      printf("Header: \n");
-      print_buf(&io_hdr_t, sizeof(io_hdr_t));
-      printf("\n");
 
       rc = ioctl(fd, SG_IO, &io_hdr_t);
 
