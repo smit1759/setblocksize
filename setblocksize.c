827a6497e426e3c8ffaf8bd15facb1a93232bea8
Carter Smith <carter.smith@stric.com.au>
2023-11-19 14:59:58 +0930

    Added other make

--- setblocksize.c
+++ setblocksize.c
@@ -158,33 +158,6 @@ struct sense_data_t
    uint8_t add_sense_bytes[0];
 };
 
-typedef struct sg_io_hdr_ibm
-{
-   int interface_id;                 /* [i] 'S' for SCSI generic (required) */
-   int dxfer_direction;              /* [i] data transfer direction  */
-   unsigned char cmd_len;            /* [i] SCSI command length ( <= 16 bytes) */
-   unsigned char mx_sb_len;          /* [i] max length to write to sbp */
-   unsigned short int iovec_count;   /* [i] 0 implies no scatter gather */
-   unsigned int dxfer_len;           /* [i] byte count of data transfer */
-   void *dxferp;                     /* [i], [*io] points to data transfer memory
-                    or scatter gather list */
-   unsigned char *cmdp;              /* [i], [*i] points to command to perform */
-   unsigned char *sbp;               /* [i], [*o] points to sense_buffer memory */
-   unsigned int timeout;             /* [i] MAX_UINT->no timeout (unit: millisec) */
-   unsigned int flags;               /* [i] 0 -> default, see SG_FLAG... */
-   int pack_id;                      /* [i->o] unused internally (normally) */
-   void *usr_ptr;                    /* [i->o] unused internally */
-   unsigned char status;             /* [o] scsi status */
-   unsigned char masked_status;      /* [o] shifted, masked scsi status */
-   unsigned char msg_status;         /* [o] messaging level data (optional) */
-   unsigned char sb_len_wr;          /* [o] byte count actually written to sbp */
-   unsigned short int host_status;   /* [o] errors from host adapter */
-   unsigned short int driver_status; /* [o] errors from software driver */
-   int resid;                        /* [o] dxfer_len - actual_transferred */
-   unsigned int duration;            /* [o] time taken by cmd (unit: millisec) */
-   unsigned int info;                /* [o] auxiliary information */
-} sg_io_hdr_t_ibm;
-
 struct df_sense_data_t
 {
    uint8_t error_code;
@@ -238,7 +211,7 @@ static int _sg_ioctl(int fd, uint8_t cdb[IPR_CCB_CDB_LEN],
       io_hdr_t.cmdp = cdb;
       io_hdr_t.dxfer_direction = data_direction;
       io_hdr_t.dxfer_len = xfer_len;
-      io_hdr_t.dxferp = &dxferp;
+      io_hdr_t.dxferp = dxferp;
       printf("Header: \n");
       print_buf(&io_hdr_t, sizeof(io_hdr_t));
       printf("\n");
